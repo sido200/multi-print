03318d7a04392e18b24d18496988c4419a88f6a7
taissir alem <taissir.alem@gmail.com>
2024-09-22 16:39:52 +0100

    link contact+ some fixes

--- src/app/services/contact.js
+++ src/app/services/contact.js
@@ -2,9 +2,6 @@ import { BASE_API_URL } from "../../config/api";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
-
-
-export const createNewsletter = (data) => {
-  return axios.post(`${BASE_API_URL}/newsletter`, data);
-}
-
+export const createContact = (data) => {
+  return axios.post(`${BASE_API_URL}/contact`, data);
+};
