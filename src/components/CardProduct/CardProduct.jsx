df4cd5dc96617d02ff62bcbe4872fd0a44a7e5bc
Wassim <512.amosimo@gmail.com>
2025-08-08 12:55:02 +0100

    image link fix

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -1,6 +1,8 @@
 import "./CardProduct.css";
 import Link from "next/link";
 import { useLocale } from "next-intl";
+import { BASE_API_URL } from './../../config/api';
+
 
 export default function CardProduct({ product }) {
   const localActive = useLocale();
@@ -8,7 +10,7 @@ export default function CardProduct({ product }) {
     <Link href={`/${localActive}/DetailsProduct/${product?._id}`}>
       <div className="card-product">
         <div className="img">
-          <img src={product?.images[0]} alt="product" />
+          <img src={BASE_API_URL.replace("/api/v1", "/images/") + product?.images[0].split("/")[product?.images[0].split("/").length - 1]} alt="product" />
         </div>
 
         <h3>{product[`title${localActive}`]}</h3>
