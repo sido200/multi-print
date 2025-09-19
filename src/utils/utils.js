0dfb80bfdb997bca6631031208c3e8033b7b4467
Wassim <512.amosimo@gmail.com>
2025-09-19 19:14:15 +0100

    .

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,7 @@
 import { BASE_API_URL } from "../config/api"
 
 export const fixLink = (link) => {
+  if(!link) return
   if(link.startsWith("blob")) return link
   return BASE_API_URL.replace("/api/v1", "/images/") + link.split("/")[link.split("/").length - 1]
 
